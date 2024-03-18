b808a8ee31230f06a64d1630fb26cccd6bdc04aa
lijiapeng001 <1146417576@qq.com>
2024-03-18 09:11:43 +0800

    feat: new

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-  printf("Hello, World!\n");
-  return 0;
+  double a, b, c,s;
+  a = 3.67;
+  b = 2.34;
+  c = 1.22;
+  s = sqrt((a + b + c) / 2);
+  printf("%f", s);
 }
